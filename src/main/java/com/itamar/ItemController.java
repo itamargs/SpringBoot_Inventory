62378958531d316e9c7ba603268afb39a2d001a3
itamar <itamargs@gmail.com>
2019-11-25 18:27:36 +0200

    IMPROVEMENT: Swagger catalog is more informative now

--- src/main/java/com/itamar/ItemController.java
+++ src/main/java/com/itamar/ItemController.java
@@ -1,5 +1,6 @@
 package com.itamar;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -9,7 +10,7 @@ import java.util.List;
 
 // This class Configures REST API
 
-@RestController // let spring know  that this is a Rest controller
+@RestController // let spring know that this is a Rest controller
 @RequestMapping("/items")
 public class ItemController {
 
@@ -17,6 +18,7 @@ public class ItemController {
     private ItemDao itemDao;
 
     // get all item in inventory
+    @ApiOperation(value = "Get list of all items", notes = "", response = Item.class)
     @RequestMapping(method = RequestMethod.GET)
     public List<Item> getAllItems() {
         return itemDao.getAllItems();
@@ -24,18 +26,21 @@ public class ItemController {
 
     // get single item by its itemNo
     //todo: handle cases when id from url isn't exist in DB
+    @ApiOperation(value = "Get specific item from the inventory given 'itemNo'", notes = "", response = Item.class)
     @RequestMapping(value = "/{itemNo}", method = RequestMethod.GET)
     public Item getItemByItemNo(@PathVariable("itemNo") int itemNo) { //get the 'id' from the url
         return itemDao.getItemByItemNo(itemNo);
     }
 
     // remove single item by its itemNo
+    @ApiOperation(value = "Delete specific item from inventory", notes = "", response = Item.class)
     @RequestMapping(value = "/{itemNo}", method = RequestMethod.DELETE)
     public void removeItemByItemNo(@PathVariable("itemNo") int itemNo) {
         itemDao.deleteItemByItemNo(itemNo);
     }
 
     // add new item (by give Item to rest API)
+    @ApiOperation(value = "Add new item (by give Item object)", notes = "", response = Item.class)
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE) //let the url contain json
     public void addItem(@RequestBody Item item) { //get the body from the rest request (the body is in json)
         itemDao.addItem(item);
@@ -44,6 +49,7 @@ public class ItemController {
 
     // remove amount of quantity by given itemNo and amount
     // usage: localhost:8080/items/withdraw/?itemNo=[itemNo]&amount=[amount]
+    @ApiOperation(value = "Removes amount of quantity by given itemNo and amount", notes = "", response = Item.class)
     @RequestMapping(value = "/withdraw", method = RequestMethod.POST)
     public void withdraw(@RequestParam int itemNo, @RequestParam int amount) { //reduce item amount quantity
         itemDao.withdraw(itemNo, amount);
@@ -51,6 +57,7 @@ public class ItemController {
 
     // add amount of quantity by given itemNo and amount
     // usage: localhost:8080/items/deposit/?itemNo=[itemNo]&amount=[amount]
+    @ApiOperation(value = "Adds amount of quantity by given itemNo and amount", notes = "", response = Item.class)
     @RequestMapping(value = "/deposit", method = RequestMethod.POST)
     public void deposit(@RequestParam int itemNo, @RequestParam int amount) { //add amount to item quantity
         itemDao.deposit(itemNo, amount);
--- src/main/java/com/itamar/Main.java
+++ src/main/java/com/itamar/Main.java
@@ -3,9 +3,18 @@ package com.itamar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 
 @EnableSwagger2 // enabling swagger
@@ -14,8 +23,30 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @ComponentScan("com") //todo: remove this line
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    // Creates Spring Bean for customize Swagger (For showing only relevant info on the HTML API catalog)
+    @Bean
+    public Docket swaggerConfiguration() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.any()) // set for presenting just our APIs
+                .apis(RequestHandlerSelectors.basePackage("com.itamar")) //set to look only on our package
+                .build()
+                .apiInfo(apiInfo());
+        //.paths(PathSelectors.ant("/items/*"))
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Inventory items -Spring Boot App",
+                "<h3>Handles inventory items </h3> \n \nUsing: Spring Boot, JPA, H2 DB, Swagger, Angular.",
+                "V1",
+                "Free to use",
+                new Contact("Itamar G", "https://github.com/itamargs/", ""),
+                "", "", Collections.emptyList());
     }
 
 }
